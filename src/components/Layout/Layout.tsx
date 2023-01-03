c1b7f7d5441793b6380f6127066cac3298e9d2b3
1eastar <ehdwls6703@gmail.com>
2023-01-03 20:08:36 +0900

    Layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,45 +1,74 @@
 /* External */
-import { useStaticQuery, graphql } from 'gatsby'
+import { useMemo } from 'react'
+import { motion } from 'framer-motion'
 
 /* Internal */
+import { isBrowser } from '@utils/browser.utils'
 import Header from '@components/Header'
+import SplitView from '@components/SplitView/SplitView'
+import SubPanel from '@components/SubPanel'
+import {
+	HOME_MAIN_PANEL_ANIMATION_VARIANTS,
+	HOME_SUB_PANEL_ANIMATION_VARIANTS,
+	POST_MAIN_PANEL_ANIMATION_VARIANTS,
+	POST_SUB_PANEL_ANIMATION_VARIANTS,
+	MAIN_PANEL_TRANSITION,
+} from './Layout.constant'
 import * as styles from './Layout.scss'
-// 스타일 import 관련 warning이 계속 뜸;;;
 
-const Layout = ({ children }) => {
-	const data = useStaticQuery(graphql`
-		query SiteTitleQuery {
-			site {
-				siteMetadata {
-					title
-				}
-			}
+interface LayoutProps {
+	children: React.ReactNode
+}
+
+function Layout({ children }: LayoutProps) {
+  const pathname = isBrowser ? window.location.pathname : ''
+	
+	const [subPanelVariants, mainPanelVariants] = useMemo(() => {
+		if(pathname === '/') {
+			return [
+				HOME_SUB_PANEL_ANIMATION_VARIANTS,
+				HOME_MAIN_PANEL_ANIMATION_VARIANTS,
+			]
+		} else {
+			return [
+				POST_SUB_PANEL_ANIMATION_VARIANTS,
+				POST_MAIN_PANEL_ANIMATION_VARIANTS,
+			]
 		}
-	`)
+	}, [pathname])
 
 	return (
 		<>
-			<Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-			<div
-				className={styles.container}
-				style={{
-					margin: `0 auto`,
-					maxWidth: `var(--size-content)`,
-					padding: `var(--size-gutter)`,
-				}}
-			>
-				<main>{children}</main>
-				<footer
-					style={{
-						marginTop: `var(--space-5)`,
-						fontSize: `var(--font-sm)`,
-					}}
-				>
-					© {new Date().getFullYear()} &middot; Built with
-					{` `}
-					<a href='https://www.gatsbyjs.com'>Gatsby</a>
-				</footer>
-			</div>
+			<Header />
+			<SplitView>
+				<div className={styles.panelContainer}>
+					<motion.span
+						layout
+						variants={subPanelVariants}
+						initial="initial"
+						animate="mounting"
+						exit="exit"
+					>
+						<SubPanel />
+					</motion.span>
+				</div>
+				<div className={styles.mainContainer}>
+					<motion.div
+						layout
+						className={styles.main}
+						variants={mainPanelVariants}
+						initial="initial"
+						animate="mounting"
+						exit="exit"
+						transition={MAIN_PANEL_TRANSITION}
+					>
+						{ children }
+					</motion.div>
+					<div className={styles.footer}>
+						© { new Date().getFullYear() } &middot; Vive kang &middot; All rights reserved
+					</div>
+				</div>
+			</SplitView>
 		</>
 	)
 }
