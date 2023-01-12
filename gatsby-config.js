ef307edd0ba875379652757cb99dea99751793f7
1eastar <ehdwls6703@gmail.com>
2023-01-12 19:25:34 +0900

    simple PWA setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,17 +32,14 @@ module.exports = {
 		{
 			resolve: 'gatsby-plugin-manifest',
 			options: {
-				name: 'gatsby-starter-default',
-				short_name: 'starter',
+				name: 'Vive Kang',
+				short_name: 'Vive',
 				start_url: '/',
-				background_color: '#663399',
-				// This will impact how browsers show your PWA/website
-				// https://css-tricks.com/meta-theme-color-and-trickery/
-				// theme_color: `#663399`,
 				display: 'minimal-ui',
-				icon: 'src/statics/images/profile.jpeg', // This path is relative to the root of the site.
+				icon: 'src/statics/images/profile.jpeg',
 			},
 		},
+		'gatsby-plugin-offline',
 		{
 			resolve: 'gatsby-plugin-mdx',
 			options: {
