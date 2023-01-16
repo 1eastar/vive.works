651a8c91e6da6597092e3570f8596c0e88a814f7
1eastar <ehdwls6703@gmail.com>
2023-01-16 14:01:09 +0900

    import path fix

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,5 +1,5 @@
 /* Internal */
-import Layout from '@components/Layout/Layout'
+import Layout from '@components/Layout'
 import '@styles/reset.css'
 
 export function wrapPageElement({ element, props }) {
--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,5 +1,5 @@
 /* Internal */
-import Layout from '@components/Layout/Layout'
+import Layout from '@components/Layout'
 
 export function wrapPageElement({ element, props }) {
   return (
