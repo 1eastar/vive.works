a9ccf06dbada9e120c77fd850e3d7d367a647122
1eastar <ehdwls6703@gmail.com>
2023-01-17 03:11:32 +0900

    add babel plugin options

--- babel.config.js
+++ babel.config.js
@@ -4,11 +4,10 @@ module.exports = function(api) {
   const presets = [
     [
       'babel-preset-gatsby',
-      // babel-preset-gatsby 안의 @babel/preset-env 프리셋이 commonjs로 변환되면 tree shaking이 일어나지 않음
-      // @babel/preset-env modules: false로 설정해주면 항상 ES 모듈로 변환되게끔 할 수 있다.
-      // default "modules: auto"
       {
-        reactRuntime: 'automatic'
+        reactRuntime: 'automatic',
+        modules: false,
+        loose: true,
       },
       'es2021',
     ],
