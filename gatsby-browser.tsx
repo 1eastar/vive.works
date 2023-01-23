231182c1d5cc11f89c8c5d6984ec68d19d3fca24
1eastar <ehdwls6703@gmail.com>
2023-01-23 17:44:08 +0900

    web font setting: Inter, Chalkboard

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,8 +1,14 @@
+/* External */
+import { WrapPageElementBrowserArgs } from 'gatsby'
+
 /* Internal */
 import Layout from '@components/Layout'
+
+import '@statics/fonts/Chalkboard/Chalkboard.css'
+import '@statics/fonts/Inter/inter.css'
 import '@styles/reset.css'
 
-export function wrapPageElement({ element, props }) {
+export function wrapPageElement({ element, props }: WrapPageElementBrowserArgs) {
   return (
     <Layout {...props}>{ element }</Layout>
   )
--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,7 +1,10 @@
+/* External */
+import { WrapPageElementNodeArgs } from 'gatsby'
+
 /* Internal */
 import Layout from '@components/Layout'
 
-export function wrapPageElement({ element, props }) {
+export function wrapPageElement({ element, props }: WrapPageElementNodeArgs) {
   return (
     <Layout {...props}>{ element }</Layout>
   )
